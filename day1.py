e3726a387bf7586987e9f2801d00eeaee978a7d2
mueen-codes <mueen.data@gmail.com>
2025-12-04 14:12:16 +0500

    Add files via upload
    Added Day 1 Python basics program

---
+++ day1.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+print("Welcome", name)
+
+subject = input("Enter your subject: ")
+marks = input("Enter your marks: ")
+
+print("Student Name:", name)
+print("Subject:", subject)
+print("Marks:", marks)
