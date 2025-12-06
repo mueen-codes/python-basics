10ed6c5208b85582e538380cb9489a512c5739f8
mueen-codes <mueen.data@gmail.com>
2025-12-06 14:15:25 +0500

    Replace student marks tracking with country list

--- day4.py
+++ day4.py
@@ -1,23 +1,7 @@
-students = []
-marks_list = []
-results = []
-
-for i in range(1,6):
-     print(f"\nStudent {i}")
-
-     name = input("Enter student name: ")
-     marks = int(input("Enter marks: "))
-
-     result = "PASS" if marks >= 40 else "FAIL"
-
-     # Data store in lists
-     students.append(name)
-     marks_list.append(marks)
-     results.append(result)
-
-# Final Output
-print("\n==== FINAL REPORT ====")
-for i in range(5):
-    print("\nStudent:",students[i])
-    print("Marks:",marks_list[i])
-    print("Result:",results[i])
+countries = ["Japan", "China", "Russia", "Canada", "Germany"]
+print(countries)
+print(countries[0])
+print(countries[-1])
+countries.append("Turkey")
+countries.remove("China")
+print(countries)
