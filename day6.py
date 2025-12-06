b4904970e83174b3383890a921b72bfdc6d65c4c
mueen-codes <mueen.data@gmail.com>
2025-12-06 14:36:50 +0500

    Completed Day 6 — Functions practice

---
+++ day6.py
@@ -0,0 +1,16 @@
+def welcome():
+    print("Welcome to Programming!")
+
+welcome()
+
+def greet(name):
+    print("Hello",name)
+
+greet("Mueen")
+greet("Ali")
+
+def add(a,b):
+    print(a+b)
+
+add(5, 7)
+add(10, 20)
