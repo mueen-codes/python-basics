9a88d393e5dc9fb45d56a38103e3cab37f6d16a8
mueen-codes <mueen.data@gmail.com>
2025-12-06 14:24:16 +0500

    Completed Day 5 (loops + lists)

---
+++ day5.py
@@ -0,0 +1,9 @@
+fruits = ["Watermelon", "Apple", "Orange", "Mango", "Banana", "Dates"]
+
+print(fruits)
+for fruit in fruits:
+    print("Fruit:",fruit)
+print(len(fruits))
+fruits.append("Grapes")
+fruits.remove("Banana")
+print(fruits)
